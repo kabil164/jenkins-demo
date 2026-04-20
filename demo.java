72c4c1bd38f6c059ae372f0635b76724319e0c6e
kabil S <kabilvishnu164@gmail.com>
2026-04-20 18:10:17 +0530

    Update output message in demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 public class demo {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins from Java");
+        System.out.println("Hello Jenkins from Javaaaa");
     }
 }
