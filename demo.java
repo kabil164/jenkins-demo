2b0c11f1b1915ac90a03fd68adbdff1f115be866
kabil S <kabilvishnu164@gmail.com>
2026-04-21 12:48:02 +0530

    Update output message in demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 public class demo {
     public static void main(String[] args) {
-        System.out.println("Hello pumooooooooooooooooo");
+        System.out.println("Hello pumooooooooooooooooo ranjiniiii");
     }
 }
