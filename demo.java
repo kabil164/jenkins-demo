870974a5213e7747e2e0dc8ae7b3f0e7a7ef4bc1
kabil S <kabilvishnu164@gmail.com>
2026-04-27 21:57:27 -0700

    Change output message in demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 public class demo {
     public static void main(String[] args) {
-        System.out.println("Hello pumooooooooooooooooo ranjiniiii");
+        System.out.println("Hello pumooooooooooooooooo kabil");
     }
 }
