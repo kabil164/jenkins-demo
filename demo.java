f511cda01153c42f0016eb65c87b9dce275790f6
kabil S <kabilvishnu164@gmail.com>
2026-06-11 12:05:55 +0530

    Simplify output message in demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 public class demo {
     public static void main(String[] args) {
-        System.out.println("Hello i am kabil frm pumoooooooooooooooo");
+        System.out.println("Hello i am kabil ");
     }
 }
